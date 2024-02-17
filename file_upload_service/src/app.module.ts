cecbb01567b69f20c83018063ec69eace323caa6
dinotong <dinotongo@gmail.com>
2024-02-18 03:18:45 +0700

    add Swagger

--- file_upload_service/src/app.module.ts
+++ file_upload_service/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { FileUploadModule } from './file_upload/file_upload.module';
 
 @Module({
-  imports: [FileUploadModule],
+  imports: [ConfigModule.forRoot(), FileUploadModule],
   controllers: [AppController],
   providers: [AppService],
 })
